f3a04891eca192614370799e791eb721fc1c4efd
Fegor200 <128870427+Fegor200@users.noreply.github.com>
2023-12-19 13:48:37 -0700

    Update alu.vhd

--- alu.vhd
+++ alu.vhd
@@ -9,7 +9,7 @@
 -- Revision 1.01 - File Modified by Raju Machupalli (October 31, 2021)
 -- Revision 2.01 - File Modified by Shyama Gandhi (November 2, 2021)
 -- Revision 3.01 - File Modified by Antonio Andara (October 31, 2023)
--- Revision 4.01 - File Modified by Oghenefegor Enwa and Shivang Duseja(December 1, 2023)
+-- Revision 4.01 - File Modified by Oghenefegor Enwa(December 1, 2023)
 -- Additional Comments:
 --******************************************************************************
 -- Total eights operations can be performed using 3 select lines of this ALU.
